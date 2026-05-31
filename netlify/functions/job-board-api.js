e4991ad321f7e7d49ce158d531d0defa56a22366
Davvvider2002 <busyinfotechng@gmail.com>
2026-05-31 22:29:09 +0200

    Add files via upload

--- netlify/functions/job-board-api.js
+++ netlify/functions/job-board-api.js
@@ -20,7 +20,7 @@ const crypto = require('crypto');
 const CORS = {
   'Access-Control-Allow-Origin':  '*',
   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-admin-token',
   'Content-Type': 'application/json',
 };
 
